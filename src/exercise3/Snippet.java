76a2732c498174b6685e3dbd4fc3843a1b2050d4
tar9code <tslbavg@gmail.com>
2019-09-04 12:50:36 -0700

    Updated Exercise 3 to have Complexity O(n)

--- src/exercise3/Snippet.java
+++
@@ -1,9 +0,0 @@
-package exercise3;
-
-public class Snippet {
-	public static void main(String[] args) {
-		
-		System.out.println(profit + " if you buy on day " + minDay + " and sell on day " + maxDay);
-	}
-}
-
